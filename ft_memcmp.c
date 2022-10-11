5da962196a470bc2cef7b93f10ad12fdd976a16f
leaque <55452110+leaque@users.noreply.github.com>
2022-10-11 20:07:49 +0200

    Add files via upload

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,28 @@
 /*   By: gkoylu <gkoylu@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/11 19:31:26 by gkoylu        #+#    #+#                 */
-/*   Updated: 2022/10/11 19:31:27 by gkoylu        ########   odam.nl         */
+/*   Updated: 2022/10/11 20:06:24 by gkoylu        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char		*b1;
+	unsigned char		*b2;
+
+	b1 = (unsigned char *)s1;
+	b2 = (unsigned char *)s2;
+	if (b1 == NULL || b2 == NULL)
+		return (0);
+	while (n)
+	{
+		if (*b1 != *b2)
+			return (*b1 - *b2);
+		b1++;
+		b2++;
+		n--;
+	}
+	return (0);
+}
